c73938722b71bb21ca1b5700f62f1fd74544233a
Philip Wang <philip27020012@gmail.com>
2022-06-16 17:41:42 +0800

    feat: add proof of work mechanism

--- src/block.js
+++ src/block.js
@@ -7,10 +7,19 @@ class Block{
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nounce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nounce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nounce += 1;
+            this.hash = this.calculateHash();
+        }
+        console.log(`Block mined: ${this.hash}`);
     }
 
 }
--- src/blockchain.js
+++ src/blockchain.js
@@ -3,11 +3,11 @@ import Block from './block.js'
 class Blockchain {
     constructor() {
         this.chain = [ this.createGenesisBlock() ];
-        
+        this.difficulty = 4;
     }
 
     createGenesisBlock(){
-        return new Block(0, '01/01/2022', 'Esuncoin');
+        return new Block(0, '01/01/2022', 'esuncoin init');
     }
 
     getLastBlock() {
@@ -16,7 +16,22 @@ class Blockchain {
 
     addBlock(newBlock) {
         newBlock.previousHash = this.getLastBlock().hash;
+        newBlock.mineBlock(this.difficulty);
         this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; ++i) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
